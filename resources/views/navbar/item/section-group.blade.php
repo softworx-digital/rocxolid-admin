dedabf495d3ff1b35c156e41e3ada7287d860c7b
Peter Bolemant <peter.bolemant@softworx.digital>
2020-05-29 10:09:16 +0200

    [in progress] cms refactoring

--- resources/views/navbar/item/section-group.blade.php
+++ resources/views/navbar/item/section-group.blade.php
@@ -1,9 +1,16 @@
 @if ($component->hasItems())
 <li @if ($component->isRouteActive()) class="active" @endif>
-    <a {{-- data-toggle="collapse" --}} href="{{ $component->getDomIdHash($loop->index, 'sub') }}"><i class="fa fa-{{ $component->getIcon() }}"></i> {{ $component->translate($component->getTitle()) }}<span class="fa fa-chevron-right"></span><span class="fa fa-chevron-down"></span></a>
+    <a {{-- data-toggle="collapse" --}} href="{{ $component->getDomIdHash($loop->index, 'sub') }}">
+        <i class="fa fa-{{ $component->getIcon() }}"></i>
+        {{ $component->translate($component->getTitle()) }}
+        <span class="fa fa-chevron-right"></span>
+        <span class="fa fa-chevron-down"></span>
+    </a>
     <ul id="{{ $component->getDomId($loop->index, 'sub') }}" class="nav child_menu">
     @foreach ($component->getItems() as $item)
-        <li @if ($item->isRouteActive()) class="active" @endif><a href="{{ $item->getRoute() }}" @if ($item->hasTarget()) target="{{ $item->getTarget() }}" @endif>{{ $item->translate($item->getTitle()) }}{{-- {{ sprintf('%s/*', $item->getRoute()) }} --}}</a></li>
+        <li @if ($item->isRouteActive()) class="active" @endif>
+            <a href="{{ $item->getRoute() }}" @if ($item->hasTarget()) target="{{ $item->getTarget() }}" @endif>{{ $item->translate($item->getTitle()) }}{{-- {{ sprintf('%s/*', $item->getRoute()) }} --}}</a>
+        </li>
     @endforeach
     </ul>
 </li>
--- resources/views/tables/table-column/model-relation.blade.php
+++ resources/views/tables/table-column/model-relation.blade.php
@@ -1,7 +1,12 @@
 @if ($user->can('viewAny', [ $component->getTableColumn()->getRelationModelClass($component->getOption('model')), $component->getTableColumn()->getRelationModelClass($component->getOption('model')) ])
     || $user->can('assign', [ $component->getOption('model'), $component->getTableColumn()->getOption('relation.name') ]))
 @if ($component->getTableColumn()->getRelationItems($component->getOption('model'))->count() > $component->getTableColumn()->getOption('relation.max-count', 5))
-<button data-toggle="collapse" data-target="{{ $component->getDomIdHash($component->getTableColumn()->getName()) }}" class="btn btn-default btn-labeled collapse-toggle collapsed" data-label-hidden="{{ $component->translate('collapse-hidden') }} ({{ $component->getTableColumn()->getRelationItems($component->getOption('model'))->count() }})" data-label-shown="{{ $component->translate('collapse-shown') }}">
+<button
+    class="btn btn-default btn-labeled collapse-toggle collapsed"
+    data-toggle="collapse"
+    data-target="{{ $component->getDomIdHash($component->getTableColumn()->getName()) }}"
+    data-label-hidden="{{ $component->translate('collapse-hidden') }} ({{ $component->getTableColumn()->getRelationItems($component->getOption('model'))->count() }})"
+    data-label-shown="{{ $component->translate('collapse-shown') }}">
     <span class="title">{{ $component->translate('collapse-hidden') }} ({{ $component->getTableColumn()->getRelationItems($component->getOption('model'))->count() }})</span>
     <span class="btn-label"><i class="glyphicon glyphicon-chevron-right"></i></span>
 </button>
@@ -10,9 +15,14 @@
 @foreach ($component->getTableColumn()->getRelationItems($component->getOption('model')) as $item)
     @if ($user->can('view', $item) || $user->can('update', $item) || $user->can('assign', [ $component->getOption('model'), $component->getTableColumn()->getOption('relation.name') ]))
         @can ('update', $item)
-            <a class="label label-info @if ($component->getTableColumn()->canUseCountryFlag($item)) has-image @endif" @if ($component->getOption('ajax', false)) data-ajax-url="{{ $item->getControllerRoute() }}" @else href="{{ $item->getControllerRoute() }}" @endif @if (isset($item->is_label_with_color) && $item->is_label_with_color && $item->color) style="background: {{ $item->color }};" @endif>
+            <a
+                class="label label-info @if ($component->getTableColumn()->canUseCountryFlag($item)) has-image @endif"
+                @if ($component->getOption('ajax', false)) data-ajax-url="{{ $item->getControllerRoute() }}" @else href="{{ $item->getControllerRoute() }}" @endif
+                @if (isset($item->is_label_with_color) && $item->is_label_with_color && $item->color) style="background: {{ $item->color }};" @endif>
         @else
-            <span class="label label-info @if ($component->getTableColumn()->canUseCountryFlag($item)) has-image @endif" @if (isset($item->is_label_with_color) && $item->is_label_with_color && $item->color) style="background: {{ $item->color }};" @endif>
+            <span
+                class="label label-info @if ($component->getTableColumn()->canUseCountryFlag($item)) has-image @endif"
+                @if (isset($item->is_label_with_color) && $item->is_label_with_color && $item->color) style="background: {{ $item->color }};" @endif>
         @endcan
         @if ($component->getTableColumn()->canUseCountryFlag($item))
             {{ Html::image(sprintf('vendor/softworx/rocXolid/images/flags/%s', $item->country->flag), $item->country->flag, [ 'class' => 'country-flag' ]) }}
