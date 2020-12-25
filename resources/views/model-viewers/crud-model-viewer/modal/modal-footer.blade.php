a203a2f01d279aed3b5df852afc4aab59c6edb64
Peter Bolemant <peter@softworx.digital>
2020-12-25 16:52:54 +0000

    general update

--- resources/views/model-viewers/crud-model-viewer/modal/modal-footer.blade.php
+++ resources/views/model-viewers/crud-model-viewer/modal/modal-footer.blade.php
@@ -1,11 +1,11 @@
 <div class="modal-footer">
     <button type="button" class="btn btn-default pull-left" data-dismiss="modal"><i class="fa fa-chevron-left margin-right-10"></i>{{ $component->translate('button.close') }}</button>
     <div class="btn-group" role="group">
-    @can ('view', $component->getModel())
-        <a href="{{ $component->getModel()->getControllerRoute('show') }}" class="btn btn-default"><i class="fa fa-eye margin-right-10"></i>{{ $component->translate('button.show') }}</a>
-    @endcan
-    {{-- @can ('update', $component->getModel())
+    @if (($display_show_button ?? true) && $user->can('view', $component->getModel()))
+        <a href="{{ $component->getModel()->getControllerRoute('show') }}" class="btn btn-info"><i class="fa fa-eye margin-right-10"></i>{{ $component->translate('button.show') }}</a>
+    @endif
+    @if (($display_edit_button ?? false) && $user->can('update', $component->getModel()))
         <a href="{{ $component->getModel()->getControllerRoute('edit') }}" class="btn btn-primary"><i class="fa fa-pencil margin-right-10"></i>{{ $component->translate('button.edit') }}</a>
-    @endcan --}}
+    @endif
     </div>
 </div>
--- resources/views/model-viewers/crud-model-viewer/modal/modal-header.blade.php
+++ resources/views/model-viewers/crud-model-viewer/modal/modal-header.blade.php
@@ -1,4 +1,4 @@
 <div class="modal-header">
     <button type="button" class="close" data-dismiss="modal"><i class="fa fa-times"></i></button>
-    <h4 class="modal-title">{{ $component->translate('model.title.singular') }} <small>{{ $component->translate(sprintf('action.%s', $route_method)) }}</small></h4>
+    <h4 class="modal-title">{{ $component->translate('model.title.singular') }}@if ($display_action ?? true) <small>{{ $component->translate(sprintf('action.%s', $route_method)) }}</small>@endif</h4>
 </div>
