21dbbdec0d3512453e81f56168f8e74662c843c5
Peter Bolemant <peter@softworx.digital>
2021-02-02 07:52:14 +0000

    general update

--- resources/views/forms/form-field/type/text.blade.php
+++ resources/views/forms/form-field/type/text.blade.php
@@ -1,5 +1,8 @@
-<div class="@if ($component->hasOption('units')) input-group margin-0 @else control-group @endif width-100">
+<div class="@if ($component->hasOption('prefix') || $component->hasOption('units')) input-group margin-0 @else control-group @endif width-100">
 {{-- @todo "hotfixed", use prefix and suffix instead --}}
+@if ($component->hasOption('prefix'))
+    <span class="input-group-addon">{!! $component->getOption('prefix') !!}</span>
+@endif
 @if ($component->getFormField()->isArray())
     {!! Form::text($component->getFormField()->getFieldName($index), $component->getFormField()->getFieldValue($index), $component->getOption('attributes')) !!}
 @else
